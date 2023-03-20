7004b3940a90587ad45fc8063b87f3a98cbeba4c
cailaxanhh <phuongnhi3120@gmail.com>
2023-03-20 15:35:06 +0700

    done with header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import logo from '../../assets/Logo.svg';
-
+import './Header.css';
+import basket from '../../assets/Basket.svg';
+import hammenu from '../../assets/icon_hamburger_menu.svg';
+import close from '../../assets/close.png';
 
 export default function Header() {
+  var x = document.getElementById("nav");
+
+  function mobile() {
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    }
+  }
+
+  function closemenu () {
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    }
+  }
+
   return (
     <div className='Header'>
+      <div className='menu'>
+        <img src={hammenu} alt='hamburger-menu' onClick={mobile}></img>
+      </div>
       <img id='header-logo' src={logo} alt="logo"></img>
-      <nav className="nav">
+      <div className='basket'>
+        <img src={basket} alt='basket'></img>
+      </div>
+      <nav id="nav">
+        <img id='close-icon' src={close} onClick={closemenu}></img>
         <ul>
           <li><a className='nav-item' href='/'>HOME</a></li>
           <li><a className='nav-item' href='/about'>ABOUT</a></li>
