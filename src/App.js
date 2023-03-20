880bd9c35345543ddee1865e860eed9937360a9d
cailaxanhh <phuongnhi3120@gmail.com>
2023-03-20 21:48:41 +0700

    done with HeroSection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from './Components/Header/Header';
+import HeroSection from './Components/HeroSection/HeroSection';
+import Hightlight from './Components/Hightlight/Hightlight';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
        <Header />
+       <HeroSection />
+       <Hightlight />
     </div>
   );
 }
--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import res from '../../assets/restaurant.jpg';
+import './HeroSection.css';
 
 export default function HeroSection() {
   return (
-    <div>HeroSection</div>
+    <div id='heroSection'>
+      <div id="heroText">
+        <h1>Little Lemon Restaurant</h1>
+        <h4>Chicago</h4>
+        <p>Little Lemon is a charming neighborhood bistro that 
+          serves simple food and classic cocktails in a lively 
+          but sasual environment. The restaurant features a 
+          locally-sourced menu with daily specials.</p>
+        <button id='reservation'>RESERVATION</button>
+      </div>
+      <img src={res} alt="restaurantpic"></img>
+    </div>
   )
 }
